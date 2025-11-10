142ed8ba7cf6f6cffde317418fd842a7ccf9a5ab
Saahillia <saahilklia22@gmail.com>
2025-11-10 13:00:13 +0530

    Fix: Updated CORS configuration for Vercel domains

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,9 +20,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser());
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
+    cors({origin: [
+        "http://localhost:5173",            
+        "",       
+        ],
+    credentials: true,
     })
 );
 
