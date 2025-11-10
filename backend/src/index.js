cfee70295a9feb81eebadc3437db21e144fd8b71
Saahillia <saahilklia22@gmail.com>
2025-11-10 13:03:56 +0530

    Fix : configuration for Vercel domains

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
     cors({origin: [
         "http://localhost:5173",            
-        "",       
+        "echo-hub-chat-app.vercel.app",       
         ],
     credentials: true,
     })
